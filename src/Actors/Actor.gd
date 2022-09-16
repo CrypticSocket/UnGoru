b2b18243d8ed28acbde19da84f198510e40dbb55
CrypticSocket <saket.vaibhav1197@gmail.com>
2022-09-17 03:25:54 +0530

    Added enemies, death and basic outline of the intro

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL = Vector2.UP
+export var gravity: = 6000.0;
+export var speed = Vector2(300, 1000)
+
+var _velocity: = Vector2.ZERO
+
+func _ready():
+	pass # Replace with function body.
+
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,23 +1,48 @@
-extends KinematicBody2D
+extends Actor
 
+export var stompVelocity: = 500.0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-var speed = Vector2(300, 1000)
-var gravity = 3000.0
-var velocity = Vector2.ZERO
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_EnemyDetector_area_entered(area):
+	_velocity = calculateStompVelocity(_velocity, stompVelocity)
+	
+func _on_EnemyDetector_body_entered(body):
+	queue_free()
+	
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	velocity.y = max(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
+	var direction: = get_direction()
+	var isJumpInterrupted: = Input.is_action_just_released("jump")
+	_velocity = calculateMoveVelocity(_velocity, direction, speed, isJumpInterrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	
+func get_direction() -> Vector2:
+	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y = -1.0 if Input.is_action_pressed("jump") and is_on_floor() else 1.0
+	return Vector2(x,y)
+
+func calculateMoveVelocity(
+	linearVelocity: Vector2,
+	direction: Vector2,
+	speed: Vector2,
+	isJumpInterrupted: bool
+):
+	var newVelocity = linearVelocity
+	newVelocity.x = speed.x * direction.x
+	newVelocity.y += gravity * get_process_delta_time()
+	if direction.y == -1.0:
+		newVelocity.y = direction.y * speed.y
+	if isJumpInterrupted and newVelocity.y < 0:
+			newVelocity.y = 0.0
+	return newVelocity
+
+func calculateStompVelocity(
+	linearVelocity: Vector2,
+	impulse: float
+) -> Vector2:
+	var stompVelocity = linearVelocity
+	stompVelocity.y = -impulse
+	return stompVelocity
+
+
