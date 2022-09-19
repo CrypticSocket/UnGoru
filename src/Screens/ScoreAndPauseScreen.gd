697533941ebf87b2bc50727ef10d619476066083
CrypticSocket <saket.vaibhav1197@gmail.com>
2022-09-19 20:33:37 +0530

    Fixed bugs

--- src/Screens/ScoreAndPauseScreen.gd
+++ src/Screens/ScoreAndPauseScreen.gd
@@ -3,9 +3,10 @@ extends Control
 onready var sceneTree: = get_tree()
 onready var pausedOverlay: ColorRect = get_node("PauseOverlay")
 onready var scoreLabel: Label = get_node("Label")
-onready var pauseText: Label = get_node("PausedText")
+onready var pauseText: Label = get_node("PauseOverlay/PausedText")
 
 var paused: = false setget set_paused
+var isPlayerDead: bool = false
 
 func _ready():
 	PlayerData.connect("scoreUpdated", self, "updateInterface")
@@ -17,12 +18,13 @@ func updateInterface():
 	
 func _onPlayerDied():
 	self.paused = true
-#	pauseText.text = "You died!!"
+	pauseText.text = "You died!!"
+	isPlayerDead = true
 	
 func _unhandled_input(event: InputEvent):
-	if event.is_action_pressed("Pause"):
+	if event.is_action_pressed("Pause") and !isPlayerDead:
 		self.paused = !paused
-		sceneTree.set_input_as_handled()
+	sceneTree.set_input_as_handled()
 
 func set_paused(value: bool):
 	paused = value
--- src/UserInterface/ChangeSceneButton.gd
+++ src/UserInterface/ChangeSceneButton.gd
@@ -3,8 +3,11 @@ extends Button
 
 export(String, FILE) var nextScenePath: = ""
 
+onready var sceneTree: = get_tree()
+
 func _on_PlayButton_button_up():
-	get_tree().change_scene(nextScenePath)
+	sceneTree.paused = false
+	sceneTree.change_scene(nextScenePath)
 
 func _get_configuration_warning():
 	return "Next Scene Path is empty" if nextScenePath == "" else ""
